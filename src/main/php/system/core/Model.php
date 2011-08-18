9219bd64963a2c93ff7222e14c635ca3f9675c8a
Endy Muhardin <endy.muhardin@gmail.com>
2011-08-18 11:50:30 +0700

    modify CI Core supaya bisa autocomplete di Eclipse.
    WARNING!! Jangan dideploy di live

--- src/main/php/system/core/Model.php
+++ src/main/php/system/core/Model.php
@@ -27,6 +27,55 @@
 class CI_Model {
 
 	/**
+	* @var CI_Config
+	*/
+	var $config;
+	/**
+	 * @var CI_DB_active_record
+	 */
+	var $db;
+	/**
+	 * @var CI_Email
+	 */
+	var $email;
+	/**
+	 * @var CI_Form_validation
+	 */
+	var $form_validation;
+	/**
+	 * @var CI_Input
+	 */
+	var $input;
+	/**
+	 * @var CI_Loader
+	 */
+	var $load;
+	/**
+	 * @var CI_Router
+	 */
+	var $router;
+	/**
+	 * @var CI_Session
+	 */
+	var $session;
+	/**
+	 * @var CI_Table
+	 */
+	var $table;
+	/**
+	 * @var CI_Unit_test
+	 */
+	var $unit;
+	/**
+	 * @var CI_URI
+	 */
+	var $uri;
+	/**
+	 * @var CI_Pagination
+	 */
+	var $pagination;
+	
+	/**
 	 * Constructor
 	 *
 	 * @access public
